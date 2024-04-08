f73783da08213fde5de454f428074de1c2b69653
juniors719 <junior.silva@alu.ufc.br>
2024-04-08 11:49:15 -0300

    reorganization

--- introducao/src/components/09PokemonContexto/PokemonContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const PokemonContext = createContext(0); // Criando o contexto
--- introducao/src/components/props/SayMyName.jsx
+++
@@ -1,9 +0,0 @@
-function SayMyName(props) {
-    return (
-        <div>
-            <h1>Fala aí, {props.name}! Suave?!</h1>
-        </div>
-    )
-}
-
-export default SayMyName;
